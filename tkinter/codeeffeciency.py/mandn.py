96a5ae68f6a059686bc20250940019c9a7568b4a
Jayarora1 <jay.arxra009@gmail.com>
2024-10-19 16:37:57 +0100

    done

---
+++ tkinter/codeeffeciency.py/mandn.py
@@ -0,0 +1,6 @@
+def num(n,m):
+    print(n*m)
+num(1,2)
+for n in range(1,10):
+    for m in range(1,10):
+        print(n*m)
---
+++ tkinter/codeeffeciency.py/name100.py
@@ -0,0 +1,9 @@
+#name = input("Please enter your name:")
+#print(f"here is your name 100 times {name*100}")
+
+#name2 = input("enter name: ")
+#for i in range(1,100):
+    #print(name2)
+def Name(name):
+    print(name*100)
+Name("Jay")
